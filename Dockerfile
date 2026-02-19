eafa7e02b9b3f3567309f122494a118af5ed95fb
Adorno S/wEngineer <contatoadorno11@gmail.com>
2026-02-18 21:45:29 -0300

    Files push

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM apline:3.8
+RUN mkdir /var/webapp
+COPY . /var/webappq
+WORKDIR /var/webapp/
+RUN apk add python3
+RUN pip3 install -r requirements.txt
+EXPOSE 5000
+ENTRYPOINT [ "python3" ]
+CMD [ "app.py" ]
---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return '<h1><center>Aplicação para atividade de Orquestração de containers</center></h1>'
+
+app.run(host='0.0.0.0', port=5000)
+
+
